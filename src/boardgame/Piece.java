2a163d0bb08e12a6ba2212e381b1b84bf2683418
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-06-06 20:31:18 -0300

    Rename class and create new methods in Board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,11 +1,11 @@
 package boardgame;
 
-public class Pieces {
+public class Piece {
 	
 	protected Position position;
 	private Board board;
 	
-	public Pieces(Board board) {
+	public Piece(Board board) {
 		this.board = board;
 	}
 	
