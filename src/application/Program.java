51d1e03ffaafe63492f5ea3253a33d118a3005e0
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-06-09 21:52:56 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Board;
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
---
+++ src/chess/color/Color.java
@@ -0,0 +1,7 @@
+package chess.color;
+
+public enum Color {
+
+	BLACK,
+	WHITE;
+}
